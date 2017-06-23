59b88c47cd5605e81342a10420b5b05df22e4694
tomasz <tomasztargo@gmail.com>
2017-06-23 13:56:00 +0200

    naniesione poprawki ale nie widać w consoli wydrukowanego drzewka

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,16 @@
 
-var parameter = 4;
-    function drawTree(parameter){
-	    for (var i = 0; i < parameter; i++) {
+var treeHeight = 4;
+    function drawTree(treeHeight){
+	    for (var i = 0; i < treeHeight; i++) {
 	    	var star = ('');
-	    	for (var j = 0; j <= i; j++){
-	    	var	star = star += '*';
-	    	}
+		    	for (var j = 0; j <= i; j++){
+		    		star + '*';
+		    	}
 	    	console.log(star);
 	  	}
 	  }
+var smallTreeHeight = 4;
+var bigTreeHeight = 10;
+
+drawTree(smallTreeHeight);
+drawTree(bigTreeHeight);
