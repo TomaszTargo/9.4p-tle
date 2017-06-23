0ed0a5d1af1efbd8ccfadb45a829a43f18666688
tomasz <tomasztargo@gmail.com>
2017-06-23 11:31:47 +0200

    zadanie wydaje się według instrukcji ale nie działa

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+
+var parameter = 4;
+    function drawTree(parameter){
+	    for (var i = 0; i < parameter; i++) {
+	    	var star = ('');
+	    	for (var j = 0; j <= i; j++){
+	    	var	star = star += '*';
+	    	}
+	    	console.log(star);
+	  	}
+	  }
